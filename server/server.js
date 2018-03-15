d9a59330f06fc6739e9104b278921299817615b8
sahar <saharbh50@gmail.com>
2018-03-15 15:22:11 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -14,7 +14,13 @@ todo.save().then((doc)=>{
 	res.status(400).send(err)
 })
 })
-
+app.get('/todos',(req,res)=>{
+	Todo.find().then((todos)=>{
+		res.send({todos})
+	},(e)=>{
+		res.status(400).send(e)
+	})
+})
 app.listen(3000,()=>{
 	console.log('started on port 3000')
 })
