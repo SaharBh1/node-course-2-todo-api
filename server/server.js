d3e281445a36fca51c1f9e26b1cc32cf7d9351f5
sahar <saharbh50@gmail.com>
2018-03-15 14:26:13 +0100

    test Post/todos route

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ todo.save().then((doc)=>{
 app.listen(3000,()=>{
 	console.log('started on port 3000')
 })
+module.exports={app}
 /*	var Todo=mongoose.model('Todo',{text:{type:String,
 		required:true,
 		minlength:1,
